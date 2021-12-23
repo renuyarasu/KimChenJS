ceeff184c6ee88adfc0e29fffa4ca4bfd8df49d9
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-23 20:55:11 +0530

    JavaScript: Primitive vs Reference

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// JComplete Modern JavaScript Firebase BootCamp The Beginner
-alert('JComplete Modern JavaScript Firebase BootCamp The Beginner')
+// JavaScript: Primitive vs Reference
+let x = 20;
+let y = x;
+console.log(x, y);
+x = 30;
+console.log(x, y);
+
+
+let a = { value: 50 }
+let b = a;
+console.log(a,b);
+a.value = 100;
+console.log(a,b);
