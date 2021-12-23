fac4d1806f472173d62bfb43a57940a8105bb0d9
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-23 22:14:06 +0530

    JavaScript: String methods and concat

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-// JavaScript: Numbers and Math Model
+// JavaScript: String methods and concat
 console.clear();
 
-const num_01 = 5;
-const num_02 = 10;
+let firstname = 'VedaGna';
+let lastname = 'Yarasu';
 let val;
 
-// Addition 
-val = num_01 + num_02
+// concatenation
+val = firstname + lastname;
+val = firstname + ' ' + lastname;
 
-// Subtraction 
-val = num_01 - num_02
+// append
+val = 'VedaGna';
+val += ' Yarasu';
 
-// Multiplication 
-val = num_01 * num_02
+// escaping
+val = "This chapter describes JavaScript's expressions and operators, including assignment, comparison, arithmetic, bitwise, logical, string, ternary and more";
+val = 'This chapter describes JavaScript\'s expressions and operators, including assignment, comparison, arithmetic, bitwise, logical, string, ternary and more';
 
-// Division 
-val = num_01 / num_02
+// length
+val = firstname.length
 
-// Remainder 
-val = num_01 % num_02
+// concat
+val = firstname.concat(' ', lastname)
 
-// Math Object 
-val = Math.PI
-val = Math.round(5.99999)
-val = Math.pow(2, 4)
-val = Math.sqrt(16)
-val = Math.abs(-555)
-val = Math.ceil(5.999)
-val = Math.floor(5.999)
-val = Math.min(1, 2, 5, 8, 9)
-val = Math.max(1, 2, 5, 8, 9)
+// change case
+val = firstname.toUpperCase()
+val = lastname.toLowerCase()
 
-// Math.random() returns a random number berween 0 (inclusive), and 1 (exclusive)
-
-val = Math.random()
-// Random between 1 to 5
-
-val = Math.floor(Math.random() * 5 + 1)
 
 console.log(val);
