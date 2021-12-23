604013a60e4a8f581be85ca2ff161e836c86202b
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-23 21:00:43 +0530

    JavaScript: Primitive vs Reference - Function

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 // JavaScript: Primitive vs Reference
-let x = 20;
-let y = x;
-console.log(x, y);
-x = 30;
-console.log(x, y);
-
-
-let a = { value: 50 }
-let b = a;
-console.log(a,b);
-a.value = 100;
-console.log(a,b);
+let user = {name: 'VedaGna'};
+function message(user){
+    user.name = 'Renu'
+    console.log('Function user: ', user.name );
+}
+message(user);
+console.log(user.name);
