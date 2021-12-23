cafae210beaed346608dd23c963ab49339ccdcd9
Renuka Prasad <renu.yarasu@gmail.com>
2021-12-23 21:38:30 +0530

    JavaScript: Convert to string

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-// JavaScript: Primitive vs Reference
-let user = {name: 'VedaGna'};
-function message(user){
-    user.name = 'Renu'
-    console.log('Function user: ', user.name );
-}
-message(user);
-console.log(user.name);
+// JavaScript: Convert to string
+console.clear();
+
+
+let val;
+
+// Numbe to string
+val = 9999;
+val = String(val)
+
+// Boolean to string
+val = true;
+val = String(val)
+
+// Date to string
+val = new Date();
+val = val.toString()
+
+// Array to string
+val = [1, 2, 3, 4, 5]
+val = String(val)
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
